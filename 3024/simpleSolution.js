1f77d7fa7e1d967ce0daa119692912cef8adf345
Muhammad Iqbal <miqbal.dev@gmail.com>
2025-05-20 00:34:11 +0800

    chore: 3024 wip

---
+++ 3024/simpleSolution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i]);
+  }
+};
+
+
+console.log([1,2,3,4])
